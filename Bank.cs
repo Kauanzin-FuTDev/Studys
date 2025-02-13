6ccf93e22bba9efb3d47dd832f57502c6e10e160
Kauan <kauan.kr1001@gmail.com>
2025-02-13 15:01:04 -0300

    Estudos c# encapsulamento

---
+++ Bank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos
+{
+    internal class Bank
+    {
+        private int Id { get; set; }
+        private double Cash { get; set; }
+        private string Name { get; set; }
+
+        public Bank(int id , string name) 
+        {
+            Id = id;
+            Name = name;  
+        }
+
+        public double Add_Cash(double morecash)
+        {
+            return Cash += morecash;
+        }
+
+        public double Out_cash(double lesscash)
+        {
+            return Cash -= lesscash;
+        }
+
+        public override string ToString()
+        {
+            if (Cash == Cash)
+            {
+                return $"ID {Id},holder's: {Name}, Cash: ${Cash} ";
+            }
+            else
+            {
+               return $"Update Data: \nID {Id}, holder's: {Name}, Cash: ${Cash}";
+            }
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Xml;
+using estudos;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string choice;
+            double morecash;
+            double lesscash;
+            
+            Console.Write("Enter the account number: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account holder's name:");
+            string name = Console.ReadLine();
+            Console.Write("Will there be an initial deposit (y/n)?");
+            choice = Console.ReadLine();
+
+            Bank bank = new Bank(id, name);
+            Choice(bank, choice);
+
+            Console.WriteLine();
+            Console.WriteLine(bank);
+            Console.WriteLine();
+
+            Console.Write("Enter the deposit amount: ");
+            morecash = double.Parse(Console.ReadLine());
+            bank.Add_Cash(morecash);
+
+            Console.WriteLine(bank);
+
+            Console.Write("Enter a withdrawal amount: ");
+            lesscash = double.Parse(Console.ReadLine());
+            bank.Out_cash(lesscash);
+
+            Console.WriteLine(bank);
+
+
+
+        }
+        static void Choice(Bank bank, string choice)
+        {
+            double morecash;
+
+            if (choice.ToLower() == "y" || choice.ToLower() == "yes")
+            {
+                Console.Write("Initial deposit: ");
+                morecash = double.Parse(Console.ReadLine());
+                bank.Add_Cash(morecash);
+            }
+        }
+}
+}
